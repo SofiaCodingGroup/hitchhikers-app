44d1e56a5fe884acdd6ce7f50bb7c75751485b11
Ivan Ivanov <ipivanov11@gmail.com>
2017-01-28 18:49:55 +0200

    facebook init params

--- src/client/app/home/home.component.ts
+++ src/client/app/home/home.component.ts
@@ -1,56 +1,67 @@
-import { Component, OnInit } from '@angular/core';
-import { NameListService } from '../shared/index';
+import {Component, OnInit} from '@angular/core';
+import {NameListService} from '../shared/index';
+import {FacebookService, FacebookInitParams, FacebookLoginResponse} from 'ng2-facebook-sdk/dist/index';
 
 /**
  * This class represents the lazy loaded HomeComponent.
  */
 @Component({
-  moduleId: module.id,
-  selector: 'sd-home',
-  templateUrl: 'home.component.html',
-  styleUrls: ['home.component.css'],
+    moduleId: module.id,
+    selector: 'sd-home',
+    templateUrl: 'home.component.html',
+    styleUrls: ['home.component.css'],
 })
 export class HomeComponent implements OnInit {
 
-  newName: string = '';
-  errorMessage: string;
-  names: any[] = [];
+    newName: string = '';
+    errorMessage: string;
+    names: any[] = [];
 
-  /**
-   * Creates an instance of the HomeComponent with the injected
-   * NameListService.
-   *
-   * @param {NameListService} nameListService - The injected NameListService.
-   */
-  constructor(public nameListService: NameListService) {}
+    /**
+     * Creates an instance of the HomeComponent with the injected
+     * NameListService.
+     *
+     * @param {NameListService} nameListService - The injected NameListService.
+     */
+    constructor(public nameListService: NameListService, private fb: FacebookService) {
+        let fbParams: FacebookInitParams = {
+            appId: '1596662717016739',
+            xfbml: true,
+            version: 'v2.8'
+        };
+        this.fb.init(fbParams).then(
+            (response: FacebookLoginResponse) => console.log(response),
+            (error: any) => console.error(error)
+        );
+    }
 
-  /**
-   * Get the names OnInit
-   */
-  ngOnInit() {
-    this.getNames();
-  }
+    /**
+     * Get the names OnInit
+     */
+    ngOnInit() {
+        this.getNames();
+    }
 
-  /**
-   * Handle the nameListService observable
-   */
-  getNames() {
-    this.nameListService.get()
-      .subscribe(
-        names => this.names = names,
-        error => this.errorMessage = <any>error
-      );
-  }
+    /**
+     * Handle the nameListService observable
+     */
+    getNames() {
+        this.nameListService.get()
+            .subscribe(
+                names => this.names = names,
+                error => this.errorMessage = <any>error
+            );
+    }
 
-  /**
-   * Pushes a new name onto the names array
-   * @return {boolean} false to prevent default form submit behavior to refresh the page.
-   */
-  addName(): boolean {
-    // TODO: implement nameListService.post
-    this.names.push(this.newName);
-    this.newName = '';
-    return false;
-  }
+    /**
+     * Pushes a new name onto the names array
+     * @return {boolean} false to prevent default form submit behavior to refresh the page.
+     */
+    addName(): boolean {
+        // TODO: implement nameListService.post
+        this.names.push(this.newName);
+        this.newName = '';
+        return false;
+    }
 
 }
--- src/client/app/home/home.module.ts
+++ src/client/app/home/home.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HomeComponent } from './home.component';
-import { HomeRoutingModule } from './home-routing.module';
-import { SharedModule } from '../shared/shared.module';
-import { NameListService } from '../shared/name-list/index';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HomeComponent} from './home.component';
+import {HomeRoutingModule} from './home-routing.module';
+import {SharedModule} from '../shared/shared.module';
+import {NameListService} from '../shared/name-list/index';
+import {FacebookService} from 'ng2-facebook-sdk/dist/index';
 
 @NgModule({
-  imports: [CommonModule, HomeRoutingModule, SharedModule],
-  declarations: [HomeComponent],
-  exports: [HomeComponent],
-  providers: [NameListService]
+    imports: [CommonModule, HomeRoutingModule, SharedModule],
+    declarations: [HomeComponent],
+    exports: [HomeComponent],
+    providers: [NameListService, FacebookService]
 })
-export class HomeModule { }
+export class HomeModule {
+}
